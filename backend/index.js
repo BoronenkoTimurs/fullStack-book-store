8f54efb1d7951a80ccb6a6e9efbeaf304a54c5bd
tboronenko <tboronenko@tboronenkos-MacBook-Air.local>
2023-12-22 13:57:28 +0200

    Add cors and give access to localhost:3000

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import bookRoute from "./routes/bookRoute.js";
+import cors from "cors";
 
 const app = express();
 // Allow to send Postman requests
@@ -10,6 +11,14 @@ app.use(express.json());
 app.get("/", (req, res, next) => {
   res.send("Hey #2!");
 });
+// Middleware for parsing request body
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use("/books", bookRoute);
 mongoose
